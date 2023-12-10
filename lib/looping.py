28aba9a2ab8b2e2abe8f2483c8b1882ede47c3a4
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-12-10 17:44:31 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+
+    counter = 0
+    while counter < 10:
+        print(counter)
+        counter -= 1
+    
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_int = [numb * numb for numb in int_list]
+    print(square_int)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    if (num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    elif (num % 3 === 0):
+        return "Fizz"
+    elif (num % 5 === 0):
+        return "Buzz"
+    else:
+        return num
+  
